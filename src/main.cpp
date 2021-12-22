be3d03206270b92df7293e78a73d1e2a3a239ce8
NingD <sam0923xd@protonmail.com>
2021-12-21 21:49:00 -0500

    commit on 'master'

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <mbed.h>
+#include <stdio.h>
 #include "gyro.h"
-
-void wait_ms(uint32_t ms)
-{
-	wait_us(1000 * ms);
-}
+/*----- uncomment this -----*/
+// #include "SPI_TFT_ILI9341.h"
+// #include "Arial12x12.h"
+// SPI_TFT_ILI9341 TFT(PF_9, PF_9, PF_7, PC_2, PE_5, PE_5);
+/*----- uncomment end -----*/
 
 // counting how many peaks are in a cycle
 // storing the average peak value.
@@ -24,13 +25,25 @@ float distance_traveled = 0;
 
 int main()
 {
+	/*----- uncomment this -----*/
+	// TFT.claim(stdout);      // send stdout to the TFT display 
+    // //TFT.claim(stderr);      // send stderr to the TFT display
+    // TFT.background(Black);    // set background to black
+    // TFT.foreground(White);    // set chars to white
+    // TFT.cls();                // clear the screen
+    // TFT.set_font((unsigned char*) Arial12x12);  // select the font
+    // TFT.set_orientation(0);
+    // printf("  Hello Mbed 0");
+	/*----- uncomment end -----*/
+
+
 	gyro_setup();
 
 	while (1)
-  	{
+	{
 		int sum = sum_of_degree();
 		float len = degree_to_velocity(1.2, sum); // setup leg length
 		distance_traveled += len * 0.5;
 		printf("Speed: %d cm/s, Distance: %d cm\n", (int)(100 * len), (int)(100*distance_traveled));
-  	}
+	}
 }
